cb580431c64fe21bcecf5678b3bd4d3f78a36938
Vladislav <anrimian@gmail.com>
2020-04-30 00:20:51 +0300

    + metadata sync interactor(2)

--- domain/src/main/java/com/github/anrimian/musicplayer/domain/interactors/sync/MetadataSyncInteractor.java
+++ domain/src/main/java/com/github/anrimian/musicplayer/domain/interactors/sync/MetadataSyncInteractor.java
@@ -3,11 +3,16 @@ package com.github.anrimian.musicplayer.domain.interactors.sync;
 import com.github.anrimian.musicplayer.domain.interactors.sync.models.FileMetadata;
 import com.github.anrimian.musicplayer.domain.interactors.sync.models.RemoteFilesMetadata;
 import com.github.anrimian.musicplayer.domain.interactors.sync.models.RemoteRepositoryType;
+import com.github.anrimian.musicplayer.domain.interactors.sync.models.RunningSyncState;
 import com.github.anrimian.musicplayer.domain.interactors.sync.repositories.RemoteRepository;
 import com.github.anrimian.musicplayer.domain.interactors.sync.repositories.RemoteStoragesRepository;
 import com.github.anrimian.musicplayer.domain.interactors.sync.repositories.SyncSettingsRepository;
 
 import java.util.List;
+
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import io.reactivex.subjects.BehaviorSubject;
 
 import static com.github.anrimian.musicplayer.domain.interactors.sync.models.RemoteRepositoryState.DISABLED_VERSION_TOO_HIGH;
 
@@ -16,6 +21,9 @@ public class MetadataSyncInteractor {
     private final int metadataVersion;
     private final SyncSettingsRepository syncSettingsRepository;
     private final RemoteStoragesRepository remoteStoragesRepository;
+
+    private final BehaviorSubject<RunningSyncState> syncStateSubject = BehaviorSubject.create();
+    private final BehaviorSubject<RemoteRepositoryType> currentSyncingRepositorySubject = BehaviorSubject.create();
 
     public MetadataSyncInteractor(int metadataVersion,
                                   SyncSettingsRepository syncSettingsRepository,
@@ -26,15 +34,39 @@ public class MetadataSyncInteractor {
     }
 
     public synchronized void runSync() {
+        Observable.fromIterable(syncSettingsRepository.getEnabledRemoteRepositories())
+                .flatMapMaybe(this::runSyncFor2)
+                //scheduler
+                //do on error.. send state in syncStateSubject? Sounds good
+                .doFinally(() -> syncStateSubject.onNext(RunningSyncState.IDLE))
+                .subscribe();
+
         for (RemoteRepositoryType repositoryType: syncSettingsRepository.getEnabledRemoteRepositories()) {
+            currentSyncingRepositorySubject.onNext(repositoryType);
             runSyncFor(repositoryType);
         }
     }
 
+    public Observable<RunningSyncState> getRunningSyncStateObservable() {
+        return syncStateSubject;
+    }
+
+    public Observable<RemoteRepositoryType> getCurrentSyncingRepositoryObservable() {
+        return currentSyncingRepositorySubject;
+    }
+
+    private Maybe<?> runSyncFor2(RemoteRepositoryType repositoryType) {
+        return Maybe.create(emitter -> {
+
+        });
+    }
+
+    //handle errors?
     private void runSyncFor(RemoteRepositoryType repositoryType) {
         RemoteRepository remoteRepository = remoteStoragesRepository.getRemoteRepository(repositoryType);
 
         //get metadata from remote
+        syncStateSubject.onNext(RunningSyncState.GET_REMOTE_METADATA);
         RemoteFilesMetadata remoteMetadata = remoteRepository.getMetadata();
 
         //check metadata version
@@ -42,8 +74,10 @@ public class MetadataSyncInteractor {
             remoteStoragesRepository.setEnabledState(repositoryType, DISABLED_VERSION_TOO_HIGH);
             return;
         }
+
         List<FileMetadata> remoteFiles = remoteMetadata.getFiles();
 
+        syncStateSubject.onNext(RunningSyncState.GET_REMOTE_FILE_TABLE);
         //get remote real file list
         //get metadata from local
         //get local real file list
---
+++ domain/src/main/java/com/github/anrimian/musicplayer/domain/interactors/sync/models/RunningSyncState.java
@@ -0,0 +1,7 @@
+package com.github.anrimian.musicplayer.domain.interactors.sync.models;
+
+public enum RunningSyncState {
+    IDLE,
+    GET_REMOTE_METADATA,
+    GET_REMOTE_FILE_TABLE
+}
